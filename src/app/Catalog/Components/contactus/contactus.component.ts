26e61c90cee103c465d9bd810052f06bf8f09c88
Ayyappan <ayyappan.akilan@gmail.com>
2024-04-21 18:32:38 +0100

    latest changes

--- src/app/Catalog/Components/contactus/contactus.component.ts
+++ src/app/Catalog/Components/contactus/contactus.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-contactus',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.css'
 })
 export class ContactusComponent {
-
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 }
